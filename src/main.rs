d88b06684fb0f46a3648b35f02daeeee11614f8c
Ryan Goldstein <ryanisaacg@gmail.com>
2018-01-20 23:53:33 -0500

    Updated to the new Screen API for screen and asset management

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[macro_use]
 extern crate quicksilver;
 
+use quicksilver::asset::*;
 use quicksilver::geom::*;
 use quicksilver::graphics::*;
 use quicksilver::input::*;
-use std::time::Duration;
+use quicksilver::*;
 
 mod enemy;
 use enemy::Enemy;
@@ -12,27 +13,61 @@ use enemy::Enemy;
 const PLAYER_RADIUS: i32 = 24;
 const PLAYER_SPEED: f32 = 5.0;
 
-pub struct State {
-    window: Window,
-    canvas: Canvas,
-    player_pos: Circle,
-    enemies: Vec<Enemy>
+pub struct LoadingScreen {
+    player: LoadingAsset<Image>,
+    crosshair: LoadingAsset<Image>
 }
 
-impl State {
-    pub fn new() -> State {
-        let (window, canvas) = WindowBuilder::new().build("Hellevator", 960, 540);
-        let player_pos = Circle::newi(100, 100, PLAYER_RADIUS);
-        let enemies = vec![Enemy::new(Circle::newi(400, 400, PLAYER_RADIUS/2)), Enemy::new(Circle::newi(300, 400, PLAYER_RADIUS/2)), Enemy::new(Circle::newi(200, 250, PLAYER_RADIUS/2))];
-        State { window, canvas, player_pos, enemies }
+impl InitialScreen for LoadingScreen {
+    fn configure() -> (Window, Canvas) {
+        WindowBuilder::new().build("Hellevator", 960, 540)
     }
 
-    pub fn events(&mut self) -> bool {
-        self.window.poll_events()
+    fn new() -> Self {
+        LoadingScreen {
+            player: Image::load("img/ah_stand.png"),
+            crosshair: Image::load("img/crosshair.png")
+        }
+    }
+}
+
+impl Screen for LoadingScreen {
+    fn update(&mut self, _window: &mut Window, _canvas: &mut Canvas) -> Option<Box<Screen>> {
+        if let LoadingAsset::Loaded(ref player_image) = self.player {
+            if let LoadingAsset::Loaded(ref crosshair) = self.crosshair {
+                let player_image = player_image.clone();
+                let crosshair = crosshair.clone();
+                let player_pos = Circle::newi(100, 100, PLAYER_RADIUS);
+                let enemies = vec![Enemy::new(Circle::newi(400, 400, PLAYER_RADIUS/2)), 
+                                   Enemy::new(Circle::newi(300, 400, PLAYER_RADIUS/2)), 
+                                   Enemy::new(Circle::newi(200, 250, PLAYER_RADIUS/2))];
+                Some(Box::new(GameScreen { player_pos, enemies, player_image, crosshair }))
+            } else {
+                self.crosshair.update();
+                None
+            }
+        } else {
+            self.player.update();
+            None
+        }
     }
 
-    pub fn update(&mut self) -> Duration {
-        let keyboard = self.window.keyboard();
+    fn draw(&mut self, window: &mut Window, canvas: &mut Canvas) {
+        canvas.clear(Color::white());
+        canvas.present(window);
+    }
+}
+
+pub struct GameScreen {
+    player_pos: Circle,
+    enemies: Vec<Enemy>,
+    player_image: Image,
+    crosshair: Image
+}
+
+impl Screen for GameScreen {
+    fn update(&mut self, window: &mut Window, _canvas: &mut Canvas) -> Option<Box<Screen>> {
+        let keyboard = window.keyboard();
         self.player_pos.x += if keyboard[Key::D].is_down() { PLAYER_SPEED } else { 0.0 };
         self.player_pos.y += if keyboard[Key::W].is_down() { -PLAYER_SPEED } else { 0.0 };
         self.player_pos.x += if keyboard[Key::A].is_down() { -PLAYER_SPEED } else { 0.0 };
@@ -40,17 +75,18 @@ impl State {
         for e in self.enemies.iter() {
             e.update();
         }
-        Duration::from_millis(16)
+        None
     }
 
-    pub fn draw(&mut self) {
-        self.canvas.clear(Color::black());
-        self.canvas.draw_circle(self.player_pos, Color::white());
+    fn draw(&mut self, window: &mut Window, canvas: &mut Canvas) {
+        canvas.clear(Color::black());
+        canvas.draw_circle(self.player_pos, Color::white());
         for e in self.enemies.iter() {
-            self.canvas.draw_circle(e.pos, Color::red());
+            canvas.draw_circle(e.pos, Color::red());
         }
-        self.canvas.present(&self.window);
+        canvas.present(window);
     }
+
 }
 
-game_loop!(State);
+screens_loop!(LoadingScreen);
