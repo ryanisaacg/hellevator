6a0ae5f79ffa4305f061b1b77d40c2950c30137b
Ryan Goldstein <ryanisaacg@gmail.com>
2018-03-21 22:04:17 -0400

    Added legs spawning when the gear legs die

--- src/boss.rs
+++ src/boss.rs
@@ -12,7 +12,6 @@ impl Boss {
             &mut Boss::Spider => {
                 enemies.push(Enemy::new(Circle::new(100, 100, 50), EnemyType::SpiderLeg(0, false)));
                 enemies.push(Enemy::new(Circle::new(100, 100, 50), EnemyType::SpiderLeg(10, false)));
-                enemies.push(Enemy::new(Circle::new(100, 100, 50), EnemyType::SpiderLeg(20, false)));
                 enemies.push(Enemy::new(Circle::new(50, 100, 50), EnemyType::GearLeg));
                 enemies.push(Enemy::new(Circle::new(960-50, 100, 50), EnemyType::GearLeg));
                 enemies.push(Enemy::new(Circle::new(50, 540-50, 50), EnemyType::GearLeg));
@@ -25,7 +24,12 @@ impl Boss {
     pub fn update(&mut self, enemies: &mut Vec<Enemy>) {
         match self {
             &mut Boss::Spider => {
-                let gears_present = enemies.iter().any(|e| e.enemy_type == EnemyType::GearLeg);
+                let gear_count = enemies.iter().filter(|e| e.enemy_type == EnemyType::GearLeg).count();
+                let leg_count = enemies.iter().filter(|e| match e.enemy_type { EnemyType::SpiderLeg(_, _) => true, _ => false }).count();
+                if 6 - gear_count != leg_count {
+                    enemies.push(Enemy::new(Circle::new(480, 260, 50), EnemyType::SpiderLeg(0, false)));
+                }
+                let gears_present = gear_count != 0;
                 for enemy in enemies.iter_mut() {
                     if let EnemyType::SpiderLeg(_, ref mut is_angry) = enemy.enemy_type {
                         *is_angry = !gears_present
